95b9e8566ceb5a44524b86f43893c87d277b4e38
FireflyStars <frostwebservice@gmail.com>
2023-10-26 11:03:00 -0700

    1
    1

--- resources/js/src/pages/dashboard/academy/dashboard.jsx
+++ resources/js/src/pages/dashboard/academy/dashboard.jsx
@@ -25,6 +25,28 @@ import { CourseCard } from '@/sections/dashboard/academy/course-card';
 import { CourseSearch } from '@/sections/dashboard/academy/course-search';
 import "./inf_finder.css"
 import { useCallback, useState } from 'react'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YoutubeIcon from '@mui/icons-material/Youtube';
+// import TiktokIcon from '@mui/icons-material/tiktok';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import LinkedinIcon from '@mui/icons-material/Linkedin';
+const TiktokIcon = ({ color = "#000000" }) => {
+  return (
+
+    <svg
+      fill={color}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 50 50"
+      width="24px"
+      height="24px"
+    >
+      <path d="M41,4H9C6.243,4,4,6.243,4,9v32c0,2.757,2.243,5,5,5h32c2.757,0,5-2.243,5-5V9C46,6.243,43.757,4,41,4z M37.006,22.323 c-0.227,0.021-0.457,0.035-0.69,0.035c-2.623,0-4.928-1.349-6.269-3.388c0,5.349,0,11.435,0,11.537c0,4.709-3.818,8.527-8.527,8.527 s-8.527-3.818-8.527-8.527s3.818-8.527,8.527-8.527c0.178,0,0.352,0.016,0.527,0.027v4.202c-0.175-0.021-0.347-0.053-0.527-0.053 c-2.404,0-4.352,1.948-4.352,4.352s1.948,4.352,4.352,4.352s4.527-1.894,4.527-4.298c0-0.095,0.042-19.594,0.042-19.594h4.016 c0.378,3.591,3.277,6.425,6.901,6.685V22.323z" />
+    </svg>
+
+
+  );
+};
 const sortOptions = [
   {
     label: 'Last update (newest)',
@@ -100,7 +122,17 @@ const Page = () => {
     { label: "Linkedin", value: "linkedin" },
   ]
   const [currentTab, setCurrentTab] = useState("instagram");
+  const renderLabel = (type) => {
+    console.log(type)
+    if (type == "Instagram") return (<><InstagramIcon style={{ display: "inline-block" }} /> <span>{type}</span></>);
+    if (type == "Youtube") return (<><YoutubeIcon style={{ display: "inline-block" }} /> <span>{type}</span></>);
+    if (type == "Tiktok") return (<><TiktokIcon color={currentTab == "tiktok" ? "#2970FF" : "grey"} style={{ display: "inline-block" }} /> <span>{type}</span></>);
+    if (type == "Twitter") return (<><TwitterIcon style={{ display: "inline-block" }} /> <span>{type}</span></>);
+    if (type == "Pinterest") return (<><PinterestIcon style={{ display: "inline-block" }} /> <span>{type}</span></>);
+    if (type == "Linkedin") return (<><LinkedinIcon style={{ display: "inline-block" }} /> <span>{type}</span></>);
 
+
+  }
   return (
     <>
       <Seo title="Dashboard: Academy Dashboard" />
@@ -148,7 +180,7 @@ const Page = () => {
                     className='custom-tab'
                     sx={{ fontSize: 18 }}
                     key={tab.value}
-                    label={tab.label}
+                    label={renderLabel(tab.label)}
                     value={tab.value}
                   />
                 ))}
